4d69dfb222d1f70523e7fc6cb7167091c0029f66
Michael Browning <mbrownin@ycp.edu>
2015-03-12 16:00:27 -0400

    Added Major class

---
+++ classSchedule/src/classSchedule/major.java
@@ -0,0 +1,48 @@
+package classSchedule;
+
+import java.util.List;
+
+
+public class major {
+	
+	private String name;
+	private boolean isMinor;
+	private List <course> requirements;
+	
+	public major (String name, boolean isMinor, List <course> requirements)
+	{
+		this.name = name;
+		this.isMinor = isMinor;
+		this.requirements = requirements;
+	}
+	
+	//Return the name of the string
+	public String getName()
+	{
+		return name;
+	}
+	
+	//Set the name of the major
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	//determine if this is a minor
+	public boolean determineMinor()
+	{
+		return isMinor;
+	}
+	
+	//Set if this is a minor
+	public void setMinor (boolean isMinor)
+	{
+		this.isMinor = isMinor;
+	}
+	
+	public List <course> getRequirements ()
+	{
+		return requirements;
+	}
+
+}
