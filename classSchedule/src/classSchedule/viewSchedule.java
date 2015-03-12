2303b4a20759b11857b8bbbbf0d32413fe0d180e
Michael Browning <mbrownin@ycp.edu>
2015-03-12 17:53:10 -0400

    work

--- classSchedule/src/classSchedule/viewSchedule.java
+++ classSchedule/src/classSchedule/viewSchedule.java
@@ -53,71 +53,71 @@ public class viewSchedule extends JFrame{
 		System.out.println("Please enter a major: ");
 		String result = keyboard.nextLine();
 
-				// Create model and controller objects
-				modelSchedule model = new modelSchedule();
-				controllerSchedule controller = new controllerSchedule();
+		// Create model and controller objects
+		modelSchedule model = new modelSchedule();
+		controllerSchedule controller = new controllerSchedule();
 
-				// Use the controller to initialize the model
-				controller.initModel(model);
+		// Use the controller to initialize the model
+		controller.initModel(model);
 
-				// Create the view, and connect it to the model and controller objects
-				viewSchedule view = new viewSchedule();
-				view.setModel(model);
-				view.setController(controller);
+		// Create the view, and connect it to the model and controller objects
+		viewSchedule view = new viewSchedule();
+		view.setModel(model);
+		view.setController(controller);
 				
-				//asks the user for a major and determines if it is a minor and prints them out
-				List<Major> listOfMajors = new ArrayList<Major>();
-				boolean isMinor;
-				Major majorAddition;
-				String result2;
-				String response;
+		//asks the user for a major and determines if it is a minor and prints them out
+		List<Major> listOfMajors = new ArrayList<Major>();
+		boolean isMinor;
+		Major majorAddition;
+		String result2;
+		String response;
 
-				do
-				{
-					System.out.println("Please enter a major: ");
-					result2 = keyboard.nextLine();
+		do
+		{
+			System.out.println("Please enter a major: ");
+			result2 = keyboard.nextLine();
 
-					System.out.println("Is this a minor?");
-					response = keyboard.nextLine();
+			System.out.println("Is this a minor?");
+			response = keyboard.nextLine();
 
-					if(response.equalsIgnoreCase("yes"))
-					{
-						isMinor = true;
-					}
-					else
-					{
-						isMinor = false;
-					}
+			if(response.equalsIgnoreCase("yes"))
+			{
+				isMinor = true;
+			}
+			else
+			{
+				isMinor = false;
+			}
 
-					majorAddition = new Major(result2, isMinor, null);
-					listOfMajors.add(majorAddition);
+			majorAddition = new Major(result2, isMinor, null);
+			listOfMajors.add(majorAddition);
 
-					System.out.println("Do you want to add another major?");
-					response = keyboard.nextLine();
-				}while(response.equalsIgnoreCase("YES"));
+			System.out.println("Do you want to add another major?");
+			response = keyboard.nextLine();
+		}while(response.equalsIgnoreCase("YES"));
 
-				System.out.println("Here is a list of all majors");
-				int numberofmajors = listOfMajors.size();
+		System.out.println("Here is a list of all majors");
+		int numberofmajors = listOfMajors.size();
 
-				Major currentMajor;
-				System.out.println("Major Name \t is a Minor");
-				for(int i = 0; i < numberofmajors; i++)
-				{
-					currentMajor = listOfMajors.get(i);
-					System.out.print(currentMajor.getName());
-					System.out.print("\t\t");
-					if(currentMajor.determineMinor())
-						System.out.print("Yes\n");
-					else
-						System.out.print("No\n");
+		Major currentMajor;
+		System.out.println("Major Name \t is a Minor");
+		for(int i = 0; i < numberofmajors; i++)
+		{
+			currentMajor = listOfMajors.get(i);
+			System.out.print(currentMajor.getName());
+			System.out.print("\t\t");
+			if(currentMajor.determineMinor())
+				System.out.print("Yes\n");
+			else
+				System.out.print("No\n");
 
-				}
+		}
 				
-				for(Course test: model.getSched())
-				{
-					System.out.println(test);
-				}
-				/*
+		for(Course test: model.getSched())
+		{
+			System.out.println(test);
+		}
+			/*
 		// Register mouse event handlers
 				 * this will be used for the search button when they type
 				 * in their major, minor, or course
@@ -131,7 +131,7 @@ public class viewSchedule extends JFrame{
 		
 		keyboard.close();
 		
-		}
-
 	}
+
+}
 
