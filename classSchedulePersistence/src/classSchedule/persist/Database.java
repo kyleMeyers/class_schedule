44af45fe5bfb4ae554d5dadcb35b4c0b1bd03325
Michael Browning <mbrownin@ycp.edu>
2015-04-07 16:54:50 -0400

    ???34

--- classSchedulePersistence/src/classSchedule/persist/Database.java
+++ classSchedulePersistence/src/classSchedule/persist/Database.java
@@ -21,272 +21,272 @@ import classSchedule.persist.DBUtil;
 //import classSchedule.persist.SqliteDatabase;
 //import classSchedule.persist.SqliteDatabase.Transaction;
 
-public class Database implements IDatabase{
-
-	@Override
-	public User findUser(String username, String password) {
-		// TODO Auto-generated method stub
-//		PreparedStatement stmt = null;
-//		ResultSet resultSet = null;
-//		
-//		try {
-//			stmt = conn.prepareStatement(
-//					"select users.* " +
-//					"  from users " +
-//					" where users.username = ? "
-//			);
-//			stmt.setString(1, username);
-//			
-//			List<User> result = new ArrayList<User>();
-//			
-//			resultSet = stmt.executeQuery();
-//			while (resultSet.next()) {
-//				User user = new User();
-//				user.setId(resultSet.getInt(1));
-//				author.setUsername(resultSet.getString(2));
-//				author.setPassword(resultSet.getString(3));
-//				
-//				result.add(user);
-//			}
-//			
-//			return result;
-//		} finally {
-//			DBUtil.closeQuietly(resultSet);
-//			DBUtil.closeQuietly(stmt);
-//		}		
-		return null;
-	}
-
-	@Override
-	public Course findCoursebyTitle(String courseName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Course findCoursebyCRN(int CRN) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Professor findProfessor(String firstname, String lastname) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Major findMajor(String major, boolean isMinor, int ID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-//	static {
-//		try {
-//			Class.forName("org.sqlite.JDBC");
-//		} catch (Exception e) {
-//			throw new IllegalStateException("Could not load sqlite driver");
-//		}
-//	}
-//	
-//	private interface Transaction<ResultType> {
-//		public ResultType execute(Connection conn) throws SQLException;
-//	}
-//
-//	private static final int MAX_ATTEMPTS = 10;
+public class Database {
 //
 //	@Override
-//	public List<Pair<Author, Book>> findAuthorAndBookByTitle(final String title) {
-//		return executeTransaction(new Transaction<List<Pair<Author,Book>>>() {
-//			@Override
-//			public List<Pair<Author, Book>> execute(Connection conn) throws SQLException {
-//				PreparedStatement stmt = null;
-//				ResultSet resultSet = null;
-//				
-//				try {
-//					stmt = conn.prepareStatement(
-//							"select authors.*, books.* " +
-//							"  from authors, books " +
-//							" where authors.id = books.author_id " +
-//							"   and books.title = ?"
-//					);
-//					stmt.setString(1, title);
-//					
-//					List<Pair<Author, Book>> result = new ArrayList<Pair<Author,Book>>();
-//					
-//					resultSet = stmt.executeQuery();
-//					while (resultSet.next()) {
-//						Author author = new Author();
-//						loadAuthor(author, resultSet, 1);
-//						Book book = new Book();
-//						loadBook(book, resultSet, 4);
-//						
-//						result.add(new Pair<Author, Book>(author, book));
-//					}
-//					
-//					return result;
-//				} finally {
-//					DBUtil.closeQuietly(resultSet);
-//					DBUtil.closeQuietly(stmt);
-//				}
-//			}
-//		});
-//	}
-//	
-//	public<ResultType> ResultType executeTransaction(Transaction<ResultType> txn) {
-//		try {
-//			return doExecuteTransaction(txn);
-//		} catch (SQLException e) {
-//			throw new PersistenceException("Transaction failed", e);
-//		}
-//	}
-//	
-//	public<ResultType> ResultType doExecuteTransaction(Transaction<ResultType> txn) throws SQLException {
-//		Connection conn = connect();
-//		
-//		try {
-//			int numAttempts = 0;
-//			boolean success = false;
-//			ResultType result = null;
-//			
-//			while (!success && numAttempts < MAX_ATTEMPTS) {
-//				try {
-//					result = txn.execute(conn);
-//					conn.commit();
-//					success = true;
-//				} catch (SQLException e) {
-//					if (e.getSQLState() != null && e.getSQLState().equals("41000")) {
-//						// Deadlock: retry (unless max retry count has been reached)
-//						numAttempts++;
-//					} else {
-//						// Some other kind of SQLException
-//						throw e;
-//					}
-//				}
-//			}
-//			
-//			if (!success) {
-//				throw new SQLException("Transaction failed (too many retries)");
-//			}
-//			
-//			// Success!
-//			return result;
-//		} finally {
-//			DBUtil.closeQuietly(conn);
-//		}
+//	public User findUser(String username, String password) {
+//		// TODO Auto-generated method stub
+////		PreparedStatement stmt = null;
+////		ResultSet resultSet = null;
+////		
+////		try {
+////			stmt = conn.prepareStatement(
+////					"select users.* " +
+////					"  from users " +
+////					" where users.username = ? "
+////			);
+////			stmt.setString(1, username);
+////			
+////			List<User> result = new ArrayList<User>();
+////			
+////			resultSet = stmt.executeQuery();
+////			while (resultSet.next()) {
+////				User user = new User();
+////				user.setId(resultSet.getInt(1));
+////				author.setUsername(resultSet.getString(2));
+////				author.setPassword(resultSet.getString(3));
+////				
+////				result.add(user);
+////			}
+////			
+////			return result;
+////		} finally {
+////			DBUtil.closeQuietly(resultSet);
+////			DBUtil.closeQuietly(stmt);
+////		}		
+//		return null;
 //	}
 //
-//	private Connection connect() throws SQLException {
-//		Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
-//		
-//		// Set autocommit to false to allow multiple the execution of
-//		// multiple queries/statements as part of the same transaction.
-//		conn.setAutoCommit(false);
-//		
-//		return conn;
+//	@Override
+//	public Course findCoursebyTitle(String courseName) {
+//		// TODO Auto-generated method stub
+//		return null;
 //	}
-//	
-//	private void loadAuthor(Author author, ResultSet resultSet, int index) throws SQLException {
-//		author.setId(resultSet.getInt(index++));
-//		author.setLastname(resultSet.getString(index++));
-//		author.setFirstname(resultSet.getString(index++));
-//	}
-//	
-//	private void loadBook(Book book, ResultSet resultSet, int index) throws SQLException {
-//		book.setId(resultSet.getInt(index++));
-//		book.setAuthorId(resultSet.getInt(index++));
-//		book.setTitle(resultSet.getString(index++));
-//		book.setIsbn(resultSet.getString(index++));
-//	}
-//	
-//	public void createTables() {
-//		executeTransaction(new Transaction<Boolean>() {
-//			@Override
-//			public Boolean execute(Connection conn) throws SQLException {
-//				PreparedStatement stmt1 = null;
-//				PreparedStatement stmt2 = null;
-//				
-//				try {
-//					stmt1 = conn.prepareStatement(
-//							"create table books (" +
-//							"    id integer primary key," +
-//							"    author_id integer," +
-//							"    title varchar(50)," +
-//							"    isbn varchar(20)" +
-//							")");
-//					stmt1.executeUpdate();
-//					
-//					stmt2 = conn.prepareStatement(
-//							"create table authors (" +
-//							"    id integer primary key," +
-//							"    lastname varchar(40)," +
-//							"    firstname varchar(40)" +
-//							")");
-//					stmt2.executeUpdate();
-//					
-//					return true;
-//				} finally {
-//					DBUtil.closeQuietly(stmt1);
-//					DBUtil.closeQuietly(stmt2);
-//				}
-//			}
-//		});
-//	}
-//	
-//	public void loadInitialData() {
-//		executeTransaction(new Transaction<Boolean>() {
-//			@Override
-//			public Boolean execute(Connection conn) throws SQLException {
-//				List<Author> authorList;
-//				List<Book> bookList;
-//				
-//				try {
-//					authorList = InitialData.getAuthors();
-//					bookList = InitialData.getBooks();
-//				} catch (IOException e) {
-//					throw new SQLException("Couldn't read initial data", e);
-//				}
 //
-//				PreparedStatement insertAuthor = null;
-//				PreparedStatement insertBook = null;
+//	@Override
+//	public Course findCoursebyCRN(int CRN) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 //
-//				try {
-//					insertAuthor = conn.prepareStatement("insert into authors values (?, ?, ?)");
-//					for (Author author : authorList) {
-//						insertAuthor.setInt(1, author.getId());
-//						insertAuthor.setString(2, author.getLastname());
-//						insertAuthor.setString(3, author.getFirstname());
-//						insertAuthor.addBatch();
-//					}
-//					insertAuthor.executeBatch();
-//					
-//					insertBook = conn.prepareStatement("insert into books values (?, ?, ?, ?)");
-//					for (Book book : bookList) {
-//						insertBook.setInt(1, book.getId());
-//						insertBook.setInt(2, book.getAuthorId());
-//						insertBook.setString(3, book.getTitle());
-//						insertBook.setString(4, book.getIsbn());
-//						insertBook.addBatch();
-//					}
-//					insertBook.executeBatch();
-//					
-//					return true;
-//				} finally {
-//					DBUtil.closeQuietly(insertBook);
-//					DBUtil.closeQuietly(insertAuthor);
-//				}
-//			}
-//		});
+//	@Override
+//	public Professor findProfessor(String firstname, String lastname) {
+//		// TODO Auto-generated method stub
+//		return null;
 //	}
-//	
-//	// The main method creates the database tables and loads the initial data.
-//	public static void main(String[] args) throws IOException {
-//		System.out.println("Creating tables...");
-//		SqliteDatabase db = new SqliteDatabase();
-//		db.createTables();
-//		
-//		System.out.println("Loading initial data...");
-//		db.loadInitialData();
-//		
-//		System.out.println("Success!");
+//
+//	@Override
+//	public Major findMajor(String major, boolean isMinor, int ID) {
+//		// TODO Auto-generated method stub
+//		return null;
 //	}
+////	static {
+////		try {
+////			Class.forName("org.sqlite.JDBC");
+////		} catch (Exception e) {
+////			throw new IllegalStateException("Could not load sqlite driver");
+////		}
+////	}
+////	
+////	private interface Transaction<ResultType> {
+////		public ResultType execute(Connection conn) throws SQLException;
+////	}
+////
+////	private static final int MAX_ATTEMPTS = 10;
+////
+////	@Override
+////	public List<Pair<Author, Book>> findAuthorAndBookByTitle(final String title) {
+////		return executeTransaction(new Transaction<List<Pair<Author,Book>>>() {
+////			@Override
+////			public List<Pair<Author, Book>> execute(Connection conn) throws SQLException {
+////				PreparedStatement stmt = null;
+////				ResultSet resultSet = null;
+////				
+////				try {
+////					stmt = conn.prepareStatement(
+////							"select authors.*, books.* " +
+////							"  from authors, books " +
+////							" where authors.id = books.author_id " +
+////							"   and books.title = ?"
+////					);
+////					stmt.setString(1, title);
+////					
+////					List<Pair<Author, Book>> result = new ArrayList<Pair<Author,Book>>();
+////					
+////					resultSet = stmt.executeQuery();
+////					while (resultSet.next()) {
+////						Author author = new Author();
+////						loadAuthor(author, resultSet, 1);
+////						Book book = new Book();
+////						loadBook(book, resultSet, 4);
+////						
+////						result.add(new Pair<Author, Book>(author, book));
+////					}
+////					
+////					return result;
+////				} finally {
+////					DBUtil.closeQuietly(resultSet);
+////					DBUtil.closeQuietly(stmt);
+////				}
+////			}
+////		});
+////	}
+////	
+////	public<ResultType> ResultType executeTransaction(Transaction<ResultType> txn) {
+////		try {
+////			return doExecuteTransaction(txn);
+////		} catch (SQLException e) {
+////			throw new PersistenceException("Transaction failed", e);
+////		}
+////	}
+////	
+////	public<ResultType> ResultType doExecuteTransaction(Transaction<ResultType> txn) throws SQLException {
+////		Connection conn = connect();
+////		
+////		try {
+////			int numAttempts = 0;
+////			boolean success = false;
+////			ResultType result = null;
+////			
+////			while (!success && numAttempts < MAX_ATTEMPTS) {
+////				try {
+////					result = txn.execute(conn);
+////					conn.commit();
+////					success = true;
+////				} catch (SQLException e) {
+////					if (e.getSQLState() != null && e.getSQLState().equals("41000")) {
+////						// Deadlock: retry (unless max retry count has been reached)
+////						numAttempts++;
+////					} else {
+////						// Some other kind of SQLException
+////						throw e;
+////					}
+////				}
+////			}
+////			
+////			if (!success) {
+////				throw new SQLException("Transaction failed (too many retries)");
+////			}
+////			
+////			// Success!
+////			return result;
+////		} finally {
+////			DBUtil.closeQuietly(conn);
+////		}
+////	}
+////
+////	private Connection connect() throws SQLException {
+////		Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+////		
+////		// Set autocommit to false to allow multiple the execution of
+////		// multiple queries/statements as part of the same transaction.
+////		conn.setAutoCommit(false);
+////		
+////		return conn;
+////	}
+////	
+////	private void loadAuthor(Author author, ResultSet resultSet, int index) throws SQLException {
+////		author.setId(resultSet.getInt(index++));
+////		author.setLastname(resultSet.getString(index++));
+////		author.setFirstname(resultSet.getString(index++));
+////	}
+////	
+////	private void loadBook(Book book, ResultSet resultSet, int index) throws SQLException {
+////		book.setId(resultSet.getInt(index++));
+////		book.setAuthorId(resultSet.getInt(index++));
+////		book.setTitle(resultSet.getString(index++));
+////		book.setIsbn(resultSet.getString(index++));
+////	}
+////	
+////	public void createTables() {
+////		executeTransaction(new Transaction<Boolean>() {
+////			@Override
+////			public Boolean execute(Connection conn) throws SQLException {
+////				PreparedStatement stmt1 = null;
+////				PreparedStatement stmt2 = null;
+////				
+////				try {
+////					stmt1 = conn.prepareStatement(
+////							"create table books (" +
+////							"    id integer primary key," +
+////							"    author_id integer," +
+////							"    title varchar(50)," +
+////							"    isbn varchar(20)" +
+////							")");
+////					stmt1.executeUpdate();
+////					
+////					stmt2 = conn.prepareStatement(
+////							"create table authors (" +
+////							"    id integer primary key," +
+////							"    lastname varchar(40)," +
+////							"    firstname varchar(40)" +
+////							")");
+////					stmt2.executeUpdate();
+////					
+////					return true;
+////				} finally {
+////					DBUtil.closeQuietly(stmt1);
+////					DBUtil.closeQuietly(stmt2);
+////				}
+////			}
+////		});
+////	}
+////	
+////	public void loadInitialData() {
+////		executeTransaction(new Transaction<Boolean>() {
+////			@Override
+////			public Boolean execute(Connection conn) throws SQLException {
+////				List<Author> authorList;
+////				List<Book> bookList;
+////				
+////				try {
+////					authorList = InitialData.getAuthors();
+////					bookList = InitialData.getBooks();
+////				} catch (IOException e) {
+////					throw new SQLException("Couldn't read initial data", e);
+////				}
+////
+////				PreparedStatement insertAuthor = null;
+////				PreparedStatement insertBook = null;
+////
+////				try {
+////					insertAuthor = conn.prepareStatement("insert into authors values (?, ?, ?)");
+////					for (Author author : authorList) {
+////						insertAuthor.setInt(1, author.getId());
+////						insertAuthor.setString(2, author.getLastname());
+////						insertAuthor.setString(3, author.getFirstname());
+////						insertAuthor.addBatch();
+////					}
+////					insertAuthor.executeBatch();
+////					
+////					insertBook = conn.prepareStatement("insert into books values (?, ?, ?, ?)");
+////					for (Book book : bookList) {
+////						insertBook.setInt(1, book.getId());
+////						insertBook.setInt(2, book.getAuthorId());
+////						insertBook.setString(3, book.getTitle());
+////						insertBook.setString(4, book.getIsbn());
+////						insertBook.addBatch();
+////					}
+////					insertBook.executeBatch();
+////					
+////					return true;
+////				} finally {
+////					DBUtil.closeQuietly(insertBook);
+////					DBUtil.closeQuietly(insertAuthor);
+////				}
+////			}
+////		});
+////	}
+////	
+////	// The main method creates the database tables and loads the initial data.
+////	public static void main(String[] args) throws IOException {
+////		System.out.println("Creating tables...");
+////		SqliteDatabase db = new SqliteDatabase();
+////		db.createTables();
+////		
+////		System.out.println("Loading initial data...");
+////		db.loadInitialData();
+////		
+////		System.out.println("Success!");
+////	}
 }
