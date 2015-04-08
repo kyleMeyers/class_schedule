f5bdc75d87fb1b46000268fb1520340ad0124302
aschmedd <aschmedd@KEC119-07>
2015-04-08 09:31:26 -0400

    Class controller, beginning of ClassServlet

--- classScheduleWebapp/src/classSchedule/servlets/ClassServlet.java
+++ classScheduleWebapp/src/classSchedule/servlets/ClassServlet.java
@@ -1,5 +1,28 @@
 package classSchedule.servlets;
 
-public class ClassServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import classSchedule.LoginController;
+import classSchedule.ClassController;
+import classSchedule.model.Course;
+import classSchedule.model.User;
+
+public class ClassServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getRequestDispatcher("/_view/class.jsp").forward(req, resp);
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException {
+		
+		
+	}
 }
