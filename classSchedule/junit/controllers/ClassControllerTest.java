53d9f23834e34914a167204166ec3ba6e125db21
Ben Coover <bcoover@ycp.edu>
2015-04-27 17:48:00 -0400

    junits (currently inoperable)

--- classSchedule/junit/controllers/ClassControllerTest.java
+++ classSchedule/junit/controllers/ClassControllerTest.java
@@ -5,35 +5,40 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import classSchedule.ClassController;
 import classSchedule.model.Major;
 import classSchedule.model.User;
 
 public class ClassControllerTest {
-
+	private User initUser, newUser;
+	private Major initMajor, newMajor;
+	private ClassController control;
+	
 	@Before
 	public void setUp() {
-		User initUser = new User();
-		User newUser = new User();
-		Major initMajor = new Major();
-		Major newMajor = new Major();
+		initUser = new User();
+		newUser = new User();
+		initMajor = new Major();
+		newMajor = new Major();
 		
-		initMajor.setMajorID("Math");
+		initMajor.setName("Math");
 		initUser.setUsername("Meyers");
 		initUser.setPassword("test1");
 		initUser.setMajor(initMajor.getName());
-		
+
+		newMajor.setName("Fun");
 		newUser.setUsername("Bob");
 		newUser.setPassword("fail");
-		newMajor.setName("Fun");
+		newUser.setMajor(newMajor.getName());
 	}
 	
 	@Test
-	public void testFindCourseByMajor() {
-		fail("Not yet implemented");
+	public void testFindCourseByMajor() throws Exception {
+		assertEquals(initMajor.getName(), control.findMajorByUser(initUser).getName());
 	}
 	
 	@Test
-	public void testFindMajorByUser() {
+	public void testFindMajorByUser() throws Exception {
 		fail("Not yet implemented");
 	}
 
