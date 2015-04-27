45c851030737d5e6e5ee0d94aad03422df9fec4c
Ben Coover <bcoover@ycp.edu>
2015-04-27 17:38:27 -0400

    junit workings

--- classSchedule/junit/controllers/ClassControllerTest.java
+++ classSchedule/junit/controllers/ClassControllerTest.java
@@ -2,12 +2,38 @@ package controllers;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import classSchedule.model.Major;
+import classSchedule.model.User;
 
 public class ClassControllerTest {
 
+	@Before
+	public void setUp() {
+		User initUser = new User();
+		User newUser = new User();
+		Major initMajor = new Major();
+		Major newMajor = new Major();
+		
+		initMajor.setMajorID("Math");
+		initUser.setUsername("Meyers");
+		initUser.setPassword("test1");
+		initUser.setMajor(initMajor.getName());
+		
+		newUser.setUsername("Bob");
+		newUser.setPassword("fail");
+		newMajor.setName("Fun");
+	}
+	
 	@Test
-	public void test() {
+	public void testFindCourseByMajor() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFindMajorByUser() {
 		fail("Not yet implemented");
 	}
 
