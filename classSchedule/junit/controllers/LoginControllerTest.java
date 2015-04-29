9d244c464ea396adc235114114dae5c88b9f954c
aschmedd <aschmedd@KEC119-07>
2015-04-29 16:23:38 -0400

    login controller junit stuff

--- classSchedule/junit/controllers/LoginControllerTest.java
+++ classSchedule/junit/controllers/LoginControllerTest.java
@@ -2,13 +2,31 @@ package controllers;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import classSchedule.LoginController;
+import classSchedule.model.User;
 
 public class LoginControllerTest {
 
+	private User user;
+	private LoginController control;
+	
+	@Before
+	public void setUp() {
+		user = new User();
+		user.setUsername("Schmedding");
+		user.setPassword("test4");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindProfessor() {
+		
+		User found = new User();
+		found = control.findUser(user.getUsername(), user.getPassword());
+		assertEquals(user.getUsername(), found.getUsername());
+		assertEquals(user.getPassword(), found.getPassword());
 	}
 
 }
