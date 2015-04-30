12103ba197837f82412565c649eb083da2288b67
kyleMeyers22 <kyleMeyers22@outlook.com>
2015-04-30 19:58:47 -0400

    cmts

--- classSchedule/src/classSchedule/persist/IDatabase.java
+++ classSchedule/src/classSchedule/persist/IDatabase.java
@@ -13,15 +13,7 @@ import classSchedule.model.User;
 
 public interface IDatabase {
 	// Persistence operations are defined here
-	
-	/**
-	 * Find the {@link User} with the given username and password. 
-	 * 
-	 * @param username the username to search for
-	 * @param password the user's password
-	 * @return the {@link User} matching the username and password,
-	 *         or null if there is no such user
-	 */
+
 	public User findUser(String username, String password);
 
 	public Professor findProfessor(String firstname, String lastname);
