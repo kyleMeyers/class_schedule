065bc6bd6acf59bf0a1183a25ca746ad44565ef5
aschmedd <aschmedd@KEC119-07>
2015-05-04 09:34:05 -0400

    professor junits

--- classScheduleUnitTests/src/controllers/ProfessorControllerTest.java
+++ classScheduleUnitTests/src/controllers/ProfessorControllerTest.java
@@ -13,6 +13,7 @@ import classSchedule.persist.FakeDatabase;
 public class ProfessorControllerTest {
 
 	private Professor prof;
+	private Professor prof2;
 	private ProfessorController control;
 	
 	@Before
@@ -20,18 +21,32 @@ public class ProfessorControllerTest {
 		FakeDatabase db = new FakeDatabase();
 		DatabaseProvider.setInstance(db);
 		control = new ProfessorController();
+		
+		//professor that exists in fake database
 		prof = new Professor();
-		prof.setFirstName("David");
+		prof.setFirstName("Dave");
 		prof.setLastName("Hovemeyer");
+		
+		//professor that doesn't exist in fake database
+		prof2 = new Professor();
+		prof2.setFirstName("No");
+		prof2.setLastName("Name");
 	}
 	
 	@Test
 	public void testFindProfessor() {
 		
+		//Professor that exists in fake database should return same first and last name
 		Professor found = new Professor();
 		found = control.findProfessor(prof.getFirstName(), prof.getLastName());
 		assertEquals(prof.getFirstName(), found.getFirstName());
 		assertEquals(prof.getLastName(), found.getLastName());
+		
+		//Professor that doesn't exist in fake database should return null
+		Professor found2 = new Professor();
+		found2 = control.findProfessor(prof2.getFirstName(), prof2.getLastName());
+		assertNull(found2.getFirstName());
+		assertNull(found2.getLastName());
 	}
 
 }
