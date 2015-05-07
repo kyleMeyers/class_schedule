3d6cb2358273311e8d130bebbe6daffd1b266bfc
Ben Coover <bcoover@ycp.edu>
2015-05-07 17:39:06 -0400

    comments

--- classScheduleUnitTests/src/controllers/ClassControllerTest.java
+++ classScheduleUnitTests/src/controllers/ClassControllerTest.java
@@ -6,15 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import classSchedule.ClassController;
-import classSchedule.model.Course;
 import classSchedule.model.Major;
 import classSchedule.model.User;
 import classSchedule.persist.DatabaseProvider;
 import classSchedule.persist.FakeDatabase;
 
 public class ClassControllerTest {
-	//private User initUser, newUser;
-	//private Major initMajor, newMajor;
 	private ClassController control;
 	
 	private User aUser;
@@ -24,21 +21,6 @@ public class ClassControllerTest {
 		FakeDatabase db = new FakeDatabase();
 		DatabaseProvider.setInstance(db);
 		control = new ClassController();
-		
-		/*initUser = new User();
-		newUser = new User();
-		initMajor = new Major();
-		newMajor = new Major();
-		
-		initMajor.setName("Math");
-		initUser.setUsername("Meyers");
-		initUser.setPassword("test1");
-		initUser.setMajor(initMajor.getName());
-
-		newMajor.setName("Fun");
-		newUser.setUsername("Bob");
-		newUser.setPassword("fail");
-		newUser.setMajor(newMajor.getName());*/
 		
 		aUser = db.findUser("Coover", "test3"); // part of the initial data
 	}
